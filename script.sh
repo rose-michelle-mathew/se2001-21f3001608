b3caa520c985106b8b0c5f33bdc05f42713d7570
rose-michelle-mathew <michelle.mathew024@gmail.com>
2024-04-27 14:57:36 +0000

    add script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Export the GitHub username
-export GITHUB_USERNAME="<rose-michelle-mathew>"
+export GITHUB_USERNAME="rose-michelle-mathew"
 
 # Export the GitHub repository name
 export GITHUB_REPOSITORY="se2001-21f3001608"
